c3ae2798ca6b7bc67ae1ec13a71e68b2edfcd0ee
ruirigel <rmrbranco@riseup.net>
2024-05-07 22:17:04 +0100

    improve code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 26
         targetSdk = 34
-        versionCode = 31032024
-        versionName = "v31032024"
+        versionCode = 20240507
+        versionName = "v20240507"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
